773bcb8712e7ef9f113a246cb35176638e3e4f60
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-11-22 21:16:28 -0200

    Initial map scroll when #wp-brasil-map is rendered

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,10 @@ jQuery(document).ready(function($) {
 	// Tooltip.
 	$( '.odin-tooltip' ).tooltip();
 
+    // Map header
+
+    if ($('#wp-brasil-map')) {
+        $('html, body').delay(1000).animate({ scrollTop: '360px' }, 1000);
+    }
+
 });
