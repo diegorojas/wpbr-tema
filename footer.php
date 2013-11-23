08b9c42281f689b4c019d0456e689955f3607ab8
Rafael Funchal <rafaelfunchal@gmail.com>
2013-11-23 02:25:23 -0200

    Some mobile adjusments

--- footer.php
+++ footer.php
@@ -16,13 +16,13 @@
 		<div class="row">
 			<div class="col-md-8 footer-area">
 				<div class="row">
-					<div class="col-md-6">
+					<div class="col-sm-6">
 						<p class="text-left"><?php echo sprintf( __( 'Powered by %s', 'comunidade-wordpress-br' ), '<a href="http://wordpress.org/" rel="nofollow" itemprop="copyrightHolder" target="_blank">' . __( 'WordPress', 'comunidade-wordpress-br' ) . '</a>' ); ?></p>
 
-					</div><!-- .col-md-6 -->
-					<div class="col-md-6">
+					</div><!-- .col-sm-6 -->
+					<div class="col-sm-6">
 						<p class="text-right"><?php _e( 'Some rights reserved', 'comunidade-wordpress-br' ); ?> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-cc.png" alt="<?php _e( 'Some rights reserved', 'comunidade-wordpress-br' ); ?>"></p>
-					</div><!-- . col-md-6 -->
+					</div><!-- . col-sm-6 -->
 				</div>
 			</div><!-- .footer-area -->
 			<div class="col-md-4 hidden-xs logo-area">
