cf8e6dfa30105fcc0ca3650d6abfdf3fdd5186b7
Rafael Funchal <rafaelfunchal@gmail.com>
2013-11-22 22:35:54 -0200

    Styling the Footer

--- footer.php
+++ footer.php
@@ -10,10 +10,33 @@
  */
 ?>
 	</div><!-- #main -->
-	<footer id="footer" role="contentinfo">
-		<span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'comunidade-wordpress-br' ); ?> | <?php echo sprintf( __( 'Powered by %s', 'comunidade-wordpress-br' ), '<a href="http://wordpress.org/" rel="nofollow" itemprop="copyrightHolder" target="_blank">' . __( 'WordPress', 'comunidade-wordpress-br' ) . '</a>' ); ?></span>
-	</footer><!-- #footer -->
 </div><!-- .container -->
+<footer id="footer" role="contentinfo">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 footer-area">
+				<div class="row">
+					<div class="col-md-6">
+						<p class="text-left"><?php echo sprintf( __( 'Powered by %s', 'comunidade-wordpress-br' ), '<a href="http://wordpress.org/" rel="nofollow" itemprop="copyrightHolder" target="_blank">' . __( 'WordPress', 'comunidade-wordpress-br' ) . '</a>' ); ?></p>
+
+					</div><!-- .col-md-6 -->
+					<div class="col-md-6">
+						<p class="text-right"><?php _e( 'Some rights reserved', 'comunidade-wordpress-br' ); ?> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-cc.png" alt="<?php _e( 'Some rights reserved', 'comunidade-wordpress-br' ); ?>"></p>
+					</div><!-- . col-md-6 -->
+				</div>
+			</div><!-- .footer-area -->
+			<div class="col-md-4 hidden-xs logo-area">
+				<img class="logo-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="<?php bloginfo('name' ); ?>">
+				<div class="logo-title">
+					<span>
+						<?php _e( 'Community', 'comunidade-wordpress-br' ); ?>
+					</span>
+					<?php _e( 'WordPress-BR', 'comunidade-wordpress-br' ); ?>
+				</div>
+			</div><!-- .logo-area -->
+		</div><!-- .row -->
+	</div><!-- .container -->
+</footer><!-- #footer -->
 <?php wp_footer(); ?>
 </body>
 </html>
