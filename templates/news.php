92251e11354de0c37a3649eea03073d71710b18c
claudiosmweb <contato@claudiosmweb.com>
2013-11-24 19:42:18 -0200

    created the news block

---
+++ templates/news.php
@@ -0,0 +1,21 @@
+<div id="archive-categories">
+	<div class="container">
+		<div class="row">
+			<div id="archive-categories-menu" class="col-md-6">
+				<h3><?php _e( 'WordPress News', 'comunidade-wordpress-br' ); ?></h3>
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'category-menu',
+								'depth'          => 1,
+								'container'      => false
+							)
+						);
+					?>
+			</div>
+			<div id="archive-categories-register" class="col-md-6">
+				<p><?php echo sprintf( __( 'Publish here you also! %s', 'comunidade-wordpress-br' ), '<a href="">' . __( 'Sign up!', 'comunidade-wordpress-br' ) . '</a>' ); ?></p>
+			</div>
+		</div>
+	</div>
+</div>
