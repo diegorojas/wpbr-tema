42412e897eb7bbcffa1159cb7fc3897196348457
edpittol <edpittol@gmail.com>
2013-11-24 22:20:32 -0200

    Responsive header

--- branding.php
+++ branding.php
@@ -11,25 +11,25 @@
             </div>
             <div id="header-buttons" class="col-md-7">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-sm-6">
                         <a href="http://br.wordpress.org/latest-pt_BR.zip" class="btn btn-primary clearfix">
                             <span class="glyphicon glyphicon-cloud-download"></span>
                             <span class="text"><?php _e( 'Last Version of<br />WordPress pt_BR', 'comunidade-wordpress-br' ) ?></span>
                         </a>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-sm-6">
                         <a href="http://mapa.wp-brasil.org/" class="btn btn-primary clearfix">
                             <span class="glyphicon glyphicon-map-marker"></span>
                             <span class="text"><?php _e( 'WordPress map<br />in Brazil', 'comunidade-wordpress-br' ) ?></span>
                         </a>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-sm-6">
                         <a href="http://br.forums.wordpress.org/" class="btn btn-primary clearfix">
                             <span class="glyphicon glyphicon-comment"></span>
                             <span class="text"><?php _e( 'WordPress.org<br />Brazilian Forum', 'comunidade-wordpress-br' ) ?></span>
                         </a>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-sm-6">
                         <a href="http://codex.wordpress.org/pt-br:P%C3%A1gina_Inicial" class="btn btn-primary clearfix">
                             <span class="glyphicon glyphicon-book"></span>
                             <span class="text"><?php _e( 'Brazilian version<br />of the Codex', 'comunidade-wordpress-br' ) ?></span>
